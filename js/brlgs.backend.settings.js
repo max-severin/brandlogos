65944c2da7c8b4f858d142ac33b4db7bfc9cc26d
Max Severin <makc.severin@gmail.com>
2015-08-09 22:24:31 +0300

    status message fix in onFormSubmit

--- js/brlgs.backend.settings.js
+++ js/brlgs.backend.settings.js
@@ -21,22 +21,27 @@ var brlgsBackendSettings = (function () { "use strict";
             
         $.post(f.attr('action'), f.serialize(), function(response) {
             if (response.status == 'ok') {
-                f.find('#wa-design-button').removeClass('red').addClass('green');
-                f.find('#wa-editor-status-fail').hide()
-                f.find('#wa-editor-status-ok span').html(response.data.message);
-                f.find('#wa-editor-status-ok').fadeIn('slow', function() {
+                $.plugins.message('success', response.data.message);
+
+                f.find('.submit .button').removeClass('red').addClass('green');
+                $("#plugins-settings-form-status").hide()
+                $("#plugins-settings-form-status span").html(response.data.message);
+                $("#plugins-settings-form-status").fadeIn('slow', function () {
                     $(this).fadeOut(1000);
                 });
+
                 if( st === 'on' ) {
                     $('#save-brlgs-form').closest('.settings-wrapper').show();
                 } else {
                     $('#save-brlgs-form').closest('.settings-wrapper').hide();
                 }
             } else {
-                f.find('#wa-design-button').removeClass('green').addClass('red');
-                f.find('#wa-editor-status-ok').hide();
-                f.find('#wa-editor-status-fail span').html(response.errors.join(', '));
-                f.find('#wa-editor-status-fail').fadeIn('slow');
+                $.plugins.message('error', response.errors || []);
+
+                f.find('.submit .button').removeClass('green').addClass('red');
+                $("#plugins-settings-form-status").hide();
+                $("#plugins-settings-form-status span").html(response.errors.join(', '));
+                $("#plugins-settings-form-status").fadeIn('slow');
             }
         }, 'json');
     };
